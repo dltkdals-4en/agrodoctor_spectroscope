d2090f30db780a34bb00fad64bf37da810676107
dltkdalsdla <dltkdalsdla@4en.co.kr>
2022-08-04 18:15:26 +0900

    [기능 추가] 기기 설정페이지

--- lib/device_connect_page.dart
+++ lib/device_connect_page.dart
@@ -1,5 +1,6 @@
 import 'package:ctgformanager/providers/ble_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:provider/provider.dart';
 
 class DeviceConnectPage extends StatelessWidget {
@@ -8,12 +9,14 @@ class DeviceConnectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var bleProvider = Provider.of<BleProvider>(context);
-    var device = bleProvider.bleDevices[bleProvider.selectedIndex!];
+    var device = bleProvider.bleDevices[bleProvider.selectedIndex!].device;
     return Scaffold(
       appBar: AppBar(
         title: Text('기기 설정'),
       ),
-      body: Container(),
+      body: Container(
+        child: Text('${device.name}'),
+      ),
     );
   }
 }
